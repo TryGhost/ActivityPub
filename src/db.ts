edc598e682c1b3097a4db49237e1cccc318c9739
Michael Barrett <mike@ghost.org>
2024-09-19 18:08:11 +0100

    Updated activities endpoint to better support client-side pagination
    no refs
    
    In order for client-side pagination to work effectively, the activities endpoint
    has been updated to:
    
    - Handle filtering required on the client-side (was previously done client-side)
    - Return nested replies in returned activities
    - Expand `inReplyTo` to full object

--- src/db.ts
+++ src/db.ts
@@ -18,24 +18,82 @@ await client.schema.createTableIfNotExists('key_value', function (table) {
     table.datetime('expires').nullable();
 });
 
-// Helper function to get the meta data for a list of activity URIs
+// Helper function to get the meta data for an array of activity URIs
 // from the database. This allows us to fetch information about the activities
 // without having to fetch the full activity object. This is a bit of a hack to
 // support sorting / filtering of the activities and should be replaced when we
 // have a proper db schema
-export async function getActivityMeta(uris: string[]): Promise<Map<string, { id: number, type: string }>> {
+
+type ActivityMeta = {
+    id: number; // Used for sorting
+    activity_type: string; // Used for filtering by activity type
+    object_type: string; // Used for filtering by object type
+    reply_object_url: string; // Used for filtering by isReplyToOwn criteria
+    reply_object_name: string; // Used for filtering by isReplyToOwn criteria
+};
+
+type getActivityMetaQueryResult = {
+    key: string,
+    left_id: number,
+    activity_type: string,
+    object_type: string,
+    reply_object_url: string,
+    reply_object_name: string
+}
+
+export async function getActivityMeta(uris: string[]): Promise<Map<string, ActivityMeta>> {
     const results = await client
-        .select('key', 'id', client.raw('JSON_EXTRACT(value, "$.type") as type'))
+        .select(
+            'left.key',
+            'left.id as left_id',
+            // mongo schmongo...
+            client.raw('JSON_EXTRACT(left.value, "$.type") as activity_type'),
+            client.raw('JSON_EXTRACT(left.value, "$.object.type") as object_type'),
+            client.raw('JSON_EXTRACT(right.value, "$.object.url") as reply_object_url'),
+            client.raw('JSON_EXTRACT(right.value, "$.object.name") as reply_object_name')
+        )
+        .from({ left: 'key_value' })
+        // @ts-ignore: This works as expected but the type definitions complain 🤔
+        .leftJoin(
+            { right: 'key_value' },
+            client.raw('JSON_UNQUOTE(JSON_EXTRACT(right.value, "$.object.id"))'),
+            '=',
+            client.raw('JSON_UNQUOTE(JSON_EXTRACT(left.value, "$.object.inReplyTo"))')
+        )
+        .whereIn('left.key', uris.map(uri => `["${uri}"]`));
+
+    const map = new Map<string, ActivityMeta>();
+
+    for (const result of results as getActivityMetaQueryResult[]) {
+        map.set(result.key.substring(2, result.key.length - 2), {
+            id: result.left_id,
+            activity_type: result.activity_type,
+            object_type: result.object_type,
+            reply_object_url: result.reply_object_url,
+            reply_object_name: result.reply_object_name,
+        });
+    }
+
+    return map;
+}
+
+// Helper function to retrieve a map of replies for an array of activity URIs
+// from the database
+export async function getRepliesMap (uris: string[]): Promise<Map<string, any>> {
+    const map = new Map<string, any>();
+
+    const results = await client
+        .select('value')
         .from('key_value')
+        .where(client.raw('JSON_EXTRACT(value, "$.object.inReplyTo") IS NOT NULL'))
         .whereIn('key', uris.map(uri => `["${uri}"]`));
 
-    const map = new Map<string, { id: number, type: string }>();
+    for (const {value: result} of results) {
+        const replies = map.get(result.object.inReplyTo) ?? [];
 
-    for (const result of results) {
-        map.set(result.key.substring(2, result.key.length - 2), {
-            id: result.id,
-            type: result.type,
-        });
+        replies.push(result);
+
+        map.set(result.object.inReplyTo, replies);
     }
 
     return map;
