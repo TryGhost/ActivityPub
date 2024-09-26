a2d7ff4fcae111eed13209f6d43841a14d386b8c
Michael Barrett <mike@ghost.org>
2024-09-26 20:20:53 +0100

    Utilise vitest for testing (#49)
    no refs
    
    Utilise vitest for testing as it is a more modern and featureful test runner
    than mocha and gives us some nice utilities out of the box (mocking, snapshots,
    coverage, etc)

--- src/example.unit.test.ts
+++ src/example.unit.test.ts
@@ -1,7 +1,7 @@
-import assert from 'assert';
+import { describe, expect, it } from 'vitest';
 
 describe('Sanity', function () {
     it('1 + 1 = 2', function () {
-        assert.equal(1 + 1, 2, '1 + 1 should equal 2');
+        expect(1 + 1).toEqual(2);
     });
 });
