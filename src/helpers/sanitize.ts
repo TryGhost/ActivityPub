179969adc7a01340d568e79e93dbfceab7156df8
Djordje Vlaisavljevic <dzvlais@gmail.com>
2024-10-23 15:01:19 +0100

    Updated sanitizeHtml rules so we can display Ghost posts properly
    ref https://linear.app/ghost/issue/AP-502/make-sanitize-html-keep-the-classes-on-tags

--- src/helpers/sanitize.ts
+++ src/helpers/sanitize.ts
@@ -6,10 +6,16 @@ export function sanitizeHtml(content: string): string {
     }
 
     return doSanitizeHtml(content, {
-        allowedTags: ['a', 'p', 'img', 'br', 'strong', 'em', 'span'],
-        allowedAttributes: {
-            a: ['href'],
-            img: ['src'],
-        }
+        allowedTags: ['address', 'img', 'article', 'aside', 'footer', 'header', 'h1', 'h2', 'h3', 'h4',
+  'h5', 'h6', 'hgroup', 'main', 'nav', 'section', 'blockquote', 'dd', 'div',
+  'dl', 'dt', 'figcaption', 'figure', 'hr', 'li', 'main', 'ol', 'p', 'pre',
+  'ul', 'a', 'abbr', 'b', 'bdi', 'bdo', 'br', 'cite', 'code', 'data', 'dfn',
+  'em', 'i', 'kbd', 'mark', 'q', 'rb', 'rp', 'rt', 'rtc', 'ruby', 's', 'samp',
+  'small', 'span', 'strong', 'sub', 'sup', 'time', 'u', 'var', 'wbr', 'caption',
+  'col', 'colgroup', 'table', 'tbody', 'td', 'tfoot', 'th', 'thead', 'tr', 'svg', 'defs', 'style', 'rect', 'polyline', 'line', 'circle', 'button', 'input', 'path', 'audio', 'video'],
+        allowedClasses: {
+            '*': false
+          },
+          allowedAttributes: false
     });
 }
