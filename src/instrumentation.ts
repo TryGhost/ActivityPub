5481e2e47d3d0ed99e4df24500c74b80e065f489
Daniel Lockyer <hi@daniellockyer.com>
2024-11-18 11:33:01 +0100

    Switched to OTel + Sentry custom setup
    ref https://docs.sentry.io/platforms/javascript/guides/node/opentelemetry/custom-setup/
    
    - I feel like I'm going back and forth with this but nothing seems to
      work flawlessly and it's quite frustrating
    - I've copied the following from the Sentry docs, so if it doesn't work
      here, I can open an issue on their repo

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,29 +1,21 @@
-import { CloudPropagator } from '@google-cloud/opentelemetry-cloud-trace-propagator';
 import {
     DiagConsoleLogger,
     DiagLogLevel,
+    type TextMapPropagator,
     diag,
-    trace,
 } from '@opentelemetry/api';
-import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
-import { NodeSDK } from '@opentelemetry/sdk-node';
+import { CompositePropagator } from '@opentelemetry/core';
 import {
     BatchSpanProcessor,
     SimpleSpanProcessor,
 } from '@opentelemetry/sdk-trace-base';
 import { NodeTracerProvider } from '@opentelemetry/sdk-trace-node';
 import * as Sentry from '@sentry/node';
-
-const sdk = new NodeSDK({
-    instrumentations: getNodeAutoInstrumentations({
-        '@opentelemetry/instrumentation-mysql2': {
-            addSqlCommenterCommentToQueries: true,
-        },
-    }),
-});
-
-const provider = new NodeTracerProvider();
-let propagator: CloudPropagator | undefined;
+import {
+    SentryPropagator,
+    SentrySampler,
+    SentrySpanProcessor,
+} from '@sentry/opentelemetry';
 
 if (process.env.NODE_ENV === 'production') {
     if (process.env.OTEL_DEBUG_LOGGING) {
@@ -33,65 +25,69 @@ if (process.env.NODE_ENV === 'production') {
     }
 }
 
-if (process.env.K_SERVICE) {
-    const { TraceExporter } = await import(
-        '@google-cloud/opentelemetry-cloud-trace-exporter'
-    );
-    provider.addSpanProcessor(new BatchSpanProcessor(new TraceExporter()));
-
-    propagator = new CloudPropagator();
-}
-
-if (process.env.NODE_ENV === 'development') {
-    const { OTLPTraceExporter } = await import(
-        '@opentelemetry/exporter-trace-otlp-proto'
-    );
-
-    provider.addSpanProcessor(
-        new SimpleSpanProcessor(
-            new OTLPTraceExporter({
-                url: 'http://jaeger:4318/v1/traces',
-            }),
-        ),
-    );
-}
-
-provider.register({
-    propagator,
-});
-
-export const tracer = trace.getTracer('activitypub');
-
-try {
-    sdk.start();
-} catch (e) {
-    console.error(e);
-}
-
 if (process.env.SENTRY_DSN) {
-    Sentry.init({
+    const sentryClient = Sentry.init({
         dsn: process.env.SENTRY_DSN,
         environment: process.env.NODE_ENV || 'unknown',
         release: process.env.K_REVISION,
+
         skipOpenTelemetrySetup: true,
+        tracesSampleRate: 1.0,
     });
+
+    const provider = new NodeTracerProvider({
+        sampler: sentryClient ? new SentrySampler(sentryClient) : undefined,
+    });
+
+    provider.addSpanProcessor(new SentrySpanProcessor());
+
+    const propagators: TextMapPropagator[] = [new SentryPropagator()];
+
+    if (process.env.K_SERVICE) {
+        const { TraceExporter } = await import(
+            '@google-cloud/opentelemetry-cloud-trace-exporter'
+        );
+        provider.addSpanProcessor(
+            new BatchSpanProcessor(new TraceExporter({})),
+        );
+
+        const { CloudPropagator } = await import(
+            '@google-cloud/opentelemetry-cloud-trace-propagator'
+        );
+        propagators.push(new CloudPropagator());
+    }
+
+    if (process.env.NODE_ENV === 'development') {
+        const { OTLPTraceExporter } = await import(
+            '@opentelemetry/exporter-trace-otlp-proto'
+        );
+        provider.addSpanProcessor(
+            new SimpleSpanProcessor(
+                new OTLPTraceExporter({
+                    url: 'http://jaeger:4318/v1/traces',
+                }),
+            ),
+        );
+    }
+
+    provider.register({
+        propagator: new CompositePropagator({ propagators }),
+        contextManager: new Sentry.SentryContextManager(),
+    });
+
+    Sentry.validateOpenTelemetrySetup();
 }
 
 export function spanWrapper<TArgs extends unknown[], TReturn>(
-    fn: (...args: TArgs) => TReturn | Promise<TReturn>,
+    fn: (...args: TArgs) => TReturn,
 ) {
     return (...args: TArgs) => {
-        return tracer.startActiveSpan(fn.name || 'anonymous', async (span) => {
-            try {
-                const result = await Promise.resolve(fn(...args));
-                span.end();
-                return result;
-            } catch (error) {
-                span.recordException(error as Error);
-                span.setStatus({ code: 2 }); // OpenTelemetry ERROR status
-                span.end();
-                throw error;
-            }
-        });
+        return Sentry.startSpan(
+            {
+                op: 'fn',
+                name: fn.name,
+            },
+            () => fn(...args),
+        );
     };
 }
