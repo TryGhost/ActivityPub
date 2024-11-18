08eb4ed54e06f4160f9caec2c82df2ec827e58f9
Daniel Lockyer <hi@daniellockyer.com>
2024-11-18 12:28:37 +0100

    Reverted all OTel changes
    ref https://github.com/TryGhost/ActivityPub/commit/a19e26f0f2b3e3968de3a801dd0a15b566954a27
    
    - this reverts the code back to the state in the commit referenced
      above, which was the last time we had tracing working well enough to
      make sense of it

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,24 +1,9 @@
-import {
-    DiagConsoleLogger,
-    DiagLogLevel,
-    type TextMapPropagator,
-    diag,
-} from '@opentelemetry/api';
-import {
-    CompositePropagator,
-    W3CTraceContextPropagator,
-} from '@opentelemetry/core';
+import { DiagConsoleLogger, DiagLogLevel, diag } from '@opentelemetry/api';
 import {
     BatchSpanProcessor,
     SimpleSpanProcessor,
 } from '@opentelemetry/sdk-trace-base';
-import { NodeTracerProvider } from '@opentelemetry/sdk-trace-node';
 import * as Sentry from '@sentry/node';
-import {
-    SentryPropagator,
-    SentrySampler,
-    SentrySpanProcessor,
-} from '@sentry/opentelemetry';
 
 if (process.env.NODE_ENV === 'production') {
     if (process.env.OTEL_DEBUG_LOGGING) {
@@ -33,39 +18,23 @@ if (process.env.SENTRY_DSN) {
         dsn: process.env.SENTRY_DSN,
         environment: process.env.NODE_ENV || 'unknown',
         release: process.env.K_REVISION,
-
-        skipOpenTelemetrySetup: true,
         tracesSampleRate: 1.0,
     });
-
-    const provider = new NodeTracerProvider({
-        sampler: sentryClient ? new SentrySampler(sentryClient) : undefined,
-    });
-
-    provider.addSpanProcessor(new SentrySpanProcessor());
-
-    const propagators: TextMapPropagator[] = [new SentryPropagator()];
 
     if (process.env.K_SERVICE) {
         const { TraceExporter } = await import(
             '@google-cloud/opentelemetry-cloud-trace-exporter'
         );
-        provider.addSpanProcessor(
+        sentryClient?.traceProvider?.addSpanProcessor(
             new BatchSpanProcessor(new TraceExporter({})),
         );
-
-        const { CloudPropagator } = await import(
-            '@google-cloud/opentelemetry-cloud-trace-propagator'
-        );
-        propagators.push(new CloudPropagator());
-        propagators.push(new W3CTraceContextPropagator());
     }
 
     if (process.env.NODE_ENV === 'development') {
         const { OTLPTraceExporter } = await import(
             '@opentelemetry/exporter-trace-otlp-proto'
         );
-        provider.addSpanProcessor(
+        sentryClient?.traceProvider?.addSpanProcessor(
             new SimpleSpanProcessor(
                 new OTLPTraceExporter({
                     url: 'http://jaeger:4318/v1/traces',
@@ -73,13 +42,6 @@ if (process.env.SENTRY_DSN) {
             ),
         );
     }
-
-    provider.register({
-        propagator: new CompositePropagator({ propagators }),
-        contextManager: new Sentry.SentryContextManager(),
-    });
-
-    Sentry.validateOpenTelemetrySetup();
 }
 
 export function spanWrapper<TArgs extends unknown[], TReturn>(
