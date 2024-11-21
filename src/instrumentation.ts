20c5a06b138d0c13215d58845f2a19e8d0513e70
Daniel Lockyer <hi@daniellockyer.com>
2024-11-21 16:31:38 +0100

    Revert "Switched to simplified version of OTel setup"
    This reverts commit e5c1547af3605af1c7ccd063558c14f026003866.

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,65 +1,59 @@
-import { TraceExporter } from '@google-cloud/opentelemetry-cloud-trace-exporter';
+import { DiagConsoleLogger, DiagLogLevel, diag } from '@opentelemetry/api';
 import {
-    DiagConsoleLogger,
-    DiagLogLevel,
-    diag,
-    trace,
-} from '@opentelemetry/api';
-import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
-import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto';
-import { NodeSDK } from '@opentelemetry/sdk-node';
+    BatchSpanProcessor,
+    SimpleSpanProcessor,
+} from '@opentelemetry/sdk-trace-base';
 import * as Sentry from '@sentry/node';
 
 if (process.env.NODE_ENV === 'production') {
     if (process.env.OTEL_DEBUG_LOGGING) {
-        diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.ALL);
+        diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG);
     } else {
         diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.INFO);
     }
 }
 
-const sdk = new NodeSDK({
-    instrumentations: getNodeAutoInstrumentations(),
-    serviceName: 'activitypub',
-    traceExporter: process.env.K_SERVICE
-        ? new TraceExporter()
-        : new OTLPTraceExporter({
-              url: 'http://jaeger:4318/v1/traces',
-          }),
-});
-
-try {
-    sdk.start();
-} catch (error) {
-    console.error('Failed to start OpenTelemetry SDK:', error);
-}
-
-const tracer = trace.getTracer('activitypub');
-
 if (process.env.SENTRY_DSN) {
-    Sentry.init({
+    const sentryClient = Sentry.init({
         dsn: process.env.SENTRY_DSN,
         environment: process.env.NODE_ENV || 'unknown',
         release: process.env.K_REVISION,
-        tracesSampleRate: 0,
-        skipOpenTelemetrySetup: true,
-        defaultIntegrations: false,
+        tracesSampleRate: 1.0,
     });
+
+    if (process.env.K_SERVICE) {
+        const { TraceExporter } = await import(
+            '@google-cloud/opentelemetry-cloud-trace-exporter'
+        );
+        sentryClient?.traceProvider?.addSpanProcessor(
+            new BatchSpanProcessor(new TraceExporter({})),
+        );
+    }
+
+    if (process.env.NODE_ENV === 'development') {
+        const { OTLPTraceExporter } = await import(
+            '@opentelemetry/exporter-trace-otlp-proto'
+        );
+        sentryClient?.traceProvider?.addSpanProcessor(
+            new SimpleSpanProcessor(
+                new OTLPTraceExporter({
+                    url: 'http://jaeger:4318/v1/traces',
+                }),
+            ),
+        );
+    }
 }
 
 export function spanWrapper<TArgs extends unknown[], TReturn>(
     fn: (...args: TArgs) => TReturn,
 ) {
     return (...args: TArgs) => {
-        return tracer.startActiveSpan(fn.name || 'anonymous', (span) => {
-            try {
-                return fn(...args);
-            } catch (error) {
-                span.recordException(error as Error);
-                throw error;
-            } finally {
-                span.end();
-            }
-        });
+        return Sentry.startSpan(
+            {
+                op: 'fn',
+                name: fn.name || 'anonymous',
+            },
+            () => fn(...args),
+        );
     };
 }
