f068f0dfb46a4a92d17552def2ddcb2dc21206b4
Michael Barrett <mike@ghost.org>
2024-11-25 13:44:42 +0000

    Added ability to run tagged cucumber scenarios (#203)
    no refs
    
    This change allows you to run only tagged scenarios when executing cucumber. This
    is useful when you want to run only a subset of scenarios

--- cucumber.js
+++ cucumber.js
@@ -1,1 +1,10 @@
-export default `--format-options '{"snippetInterface": "synchronous"}'`;
+let cmd = `--format-options '{"snippetInterface": "synchronous"}'`;
+
+if (process.env.TAGS) {
+    // @see https://cucumber.io/docs/cucumber/api/?lang=javascript#tags
+    cmd += ` --tags '${process.env.TAGS}'`;
+}
+
+console.log(cmd);
+
+export default cmd;
