4d711fd94177228e071b719940402416b0ffeca3
Michael Barrett <mike@ghost.org>
2024-11-29 22:23:35 +0000

    Added endpoint for retrieving profile posts
    no refs
    
    Added endpoint for retrieving profile posts

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,4 +3,5 @@ export { getActivityThreadAction } from './action/getActivityThread';
 export { profileGetAction } from './action/profile/get';
 export { profileGetFollowersAction } from './action/profile/getFollowers';
 export { profileGetFollowingAction } from './action/profile/getFollowing';
+export { profileGetPostsAction } from './action/profile/getPosts';
 export { searchAction } from './action/search';
