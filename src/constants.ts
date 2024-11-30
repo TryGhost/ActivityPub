136be79b9cb2ed3ded000e62431a9beb42be447f
Michael Barrett <mike@ghost.org>
2024-11-30 15:52:03 +0000

    Reduced size of activitypub collection page (#219)
    no refs
    
    Reduced the number of items that are returned when fetching an activitpub
    collection page

--- src/constants.ts
+++ src/constants.ts
@@ -3,7 +3,7 @@ export const ACTOR_DEFAULT_NAME = 'Local Ghost site';
 export const ACTOR_DEFAULT_ICON = 'https://ghost.org/favicon.ico';
 export const ACTOR_DEFAULT_SUMMARY = 'This is a summary';
 
-export const FOLLOWERS_PAGE_SIZE = 100;
-export const FOLLOWING_PAGE_SIZE = 100;
-export const LIKED_PAGE_SIZE = 100;
-export const OUTBOX_PAGE_SIZE = 100;
+export const FOLLOWERS_PAGE_SIZE = 20;
+export const FOLLOWING_PAGE_SIZE = 20;
+export const LIKED_PAGE_SIZE = 20;
+export const OUTBOX_PAGE_SIZE = 20;
