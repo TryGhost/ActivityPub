d2aaac4cf665cf38a6c73cf8cf2c6f4f51ae7533
Michael Barrett <mike@ghost.org>
2025-01-09 16:04:39 +0000

    Moved webhook actions to API (#254)
    no refs
    
    Moved webhook actions to API for easier maintenance and consistency with other
    API actions

--- src/api/index.ts
+++ src/api/index.ts
@@ -5,3 +5,5 @@ export { profileGetFollowersAction } from './action/profile/get-followers';
 export { profileGetFollowingAction } from './action/profile/get-following';
 export { profileGetPostsAction } from './action/profile/get-posts';
 export { searchAction } from './action/search';
+export { webookPostPublishedAction } from './action/webhook/post-published';
+export { webhookSiteChangedAction } from './action/webhook/site-changed';
