ac9841ba048028e674fddbf5842ddbb2d1a9f490
Fabien O'Carroll <fabien@allou.is>
2025-02-13 13:40:00 +0700

    Added support for liking to the Post entity
    refs https://linear.app/ghost/issue/AP-728
    
    This adds the initial support for liking a Post. It's a bit tricky because of
    how messy AP is - we need to be able handle duplicate likes for example we may
    be forwarded multiple of the same Like or have likes come in via the Inbox as
    well as our internal API's.
    
    We also need to work out how to handle the like count - whether we increment
    that in place on the model, which could be wrong. Or just update it on
    persistence, which is more accurate but then do we need to mutate the entity in
    the save method? We can work this out as we go, with a federated network we're
    not expected to have exact numbers for these things.

--- src/post/post.repository.knex.integration.test.ts
+++ src/post/post.repository.knex.integration.test.ts
@@ -55,4 +55,132 @@ describe('KnexPostRepository', () => {
 
         assert(rowInDb, 'A row should have been saved in the DB');
     });
+
+    it('Handles likes of a new post', async () => {
+        const events = new EventEmitter();
+        const accountService = new AccountService(client, events);
+        const siteService = new SiteService(client, accountService, {
+            async getSiteSettings(host: string) {
+                return {
+                    site: {
+                        title: 'Test Site',
+                        description: 'A fake site used for testing',
+                        icon: 'https://testing.com/favicon.ico',
+                    },
+                };
+            },
+        });
+        const accountRepository = new KnexAccountRepository(client, events);
+        const postRepository = new KnexPostRepository(client, events);
+
+        async function getAccount(host: string) {
+            const site = await siteService.initialiseSiteForHost(host);
+
+            const account = await accountRepository.getBySite(site);
+
+            return account;
+        }
+
+        const accounts = await Promise.all(
+            ['testing-one.com', 'testing-two.com', 'testing-three.com'].map(
+                getAccount,
+            ),
+        );
+
+        const post = Post.createArticleFromGhostPost(accounts[0], {
+            title: 'Title',
+            html: '<p>Hello, world!</p>',
+            excerpt: 'Hello, world!',
+            feature_image: null,
+            url: 'https://testing.com/hello-world',
+            published_at: '2025-01-01',
+        });
+
+        post.addLike(accounts[0]);
+        post.addLike(accounts[1]);
+        post.addLike(accounts[2]);
+
+        await postRepository.save(post);
+
+        const rowInDb = await client('posts')
+            .where({
+                uuid: post.uuid,
+            })
+            .select('*')
+            .first();
+
+        assert(rowInDb, 'A row should have been saved in the DB');
+
+        const likesInDb = await client('likes')
+            .where({
+                post_id: post.id,
+            })
+            .select('*');
+
+        assert.equal(likesInDb.length, 3, 'There should be 3 likes in the DB');
+    });
+
+    it('Handles likes of an existing post', async () => {
+        const events = new EventEmitter();
+        const accountService = new AccountService(client, events);
+        const siteService = new SiteService(client, accountService, {
+            async getSiteSettings(host: string) {
+                return {
+                    site: {
+                        title: 'Test Site',
+                        description: 'A fake site used for testing',
+                        icon: 'https://testing.com/favicon.ico',
+                    },
+                };
+            },
+        });
+        const accountRepository = new KnexAccountRepository(client, events);
+        const postRepository = new KnexPostRepository(client, events);
+
+        async function getAccount(host: string) {
+            const site = await siteService.initialiseSiteForHost(host);
+
+            const account = await accountRepository.getBySite(site);
+
+            return account;
+        }
+
+        const accounts = await Promise.all(
+            ['testing-one.com', 'testing-two.com', 'testing-three.com'].map(
+                getAccount,
+            ),
+        );
+
+        const post = Post.createArticleFromGhostPost(accounts[0], {
+            title: 'Title',
+            html: '<p>Hello, world!</p>',
+            excerpt: 'Hello, world!',
+            feature_image: null,
+            url: 'https://testing.com/hello-world',
+            published_at: '2025-01-01',
+        });
+
+        post.addLike(accounts[1]);
+
+        await postRepository.save(post);
+
+        post.addLike(accounts[0]);
+        post.addLike(accounts[1]);
+
+        await postRepository.save(post);
+
+        post.addLike(accounts[0]);
+        post.addLike(accounts[2]);
+
+        await postRepository.save(post);
+
+        const rowInDb = await client('posts')
+            .where({
+                uuid: post.uuid,
+            })
+            .select('like_count')
+            .first();
+
+        assert.equal(rowInDb.like_count, 3, 'There should be 3 likes');
+    });
 });
