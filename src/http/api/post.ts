53762973590825f6240823737b299c29c4b2fca4
Princi Vershwal <vershwal.princi@gmail.com>
2025-03-06 12:02:02 +0530

    Fixed lint errors

--- src/http/api/post.ts
+++ src/http/api/post.ts
@@ -1,12 +1,12 @@
-import type { KnexAccountRepository } from 'account/account.repository.knex';
 import { createHash } from 'node:crypto';
-import { fedify } from '../../app';
-import { parseURL } from 'core/url';
-import { removeFromList } from '../../kv-helpers';
-import type { AppContext } from '../../app';
 import { Like } from '@fedify/fedify';
+import type { KnexAccountRepository } from 'account/account.repository.knex';
+import { parseURL } from 'core/url';
 import type { KnexPostRepository } from 'post/post.repository.knex';
 import type { PostService } from 'post/post.service';
+import { fedify } from '../../app';
+import type { AppContext } from '../../app';
+import { removeFromList } from '../../kv-helpers';
 
 /**
  * Create a handler for a request to delete a post
