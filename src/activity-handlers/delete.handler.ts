cc8d39b6f850794161cb290f0a426fada73e10a2
Princi Vershwal <vershwal.princi@gmail.com>
2025-03-10 13:07:45 +0530

    Handling Post/Account not found error for incoming delete (#375)
    - We need to wrap the getByApId in a try/catch and if it cannot find the post, have it just return
    - The reason being that a Delete activity can come in for a post which we don't know of, but when we try to fetch it, we can't, because it's been deleted!

--- src/activity-handlers/delete.handler.ts
+++ src/activity-handlers/delete.handler.ts
@@ -1,6 +1,8 @@
 import type { Context, Delete } from '@fedify/fedify';
+import type { Account } from 'account/account.entity';
 import type { AccountService } from 'account/account.service';
 import type { ContextData } from 'app';
+import type { Post } from 'post/post.entity';
 import type { KnexPostRepository } from 'post/post.repository.knex';
 import type { PostService } from 'post/post.service';
 import { getRelatedActivities } from '../db';
@@ -33,14 +35,28 @@ export class DeleteHandler {
             return;
         }
 
-        const senderAccount = await this.accountService.getByApId(sender.id);
+        let senderAccount: Account | null = null;
+
+        try {
+            senderAccount = await this.accountService.getByApId(sender.id);
+        } catch (error) {
+            ctx.data.logger.error('Error fetching sender account', { error });
+            return;
+        }
 
         if (senderAccount === null) {
             ctx.data.logger.info('Sender account not found, exit early');
             return;
         }
 
-        const post = await this.postService.getByApId(deleteActivity.objectId);
+        let post: Post | null = null;
+
+        try {
+            post = await this.postService.getByApId(deleteActivity.objectId);
+        } catch (error) {
+            ctx.data.logger.error('Error fetching post', { error });
+            return;
+        }
 
         if (post === null) {
             ctx.data.logger.info('Post not found, exit early');
