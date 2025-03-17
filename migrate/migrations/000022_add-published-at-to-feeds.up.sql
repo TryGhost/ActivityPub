50e4d5c071a5fb34fb953855c2be7ddcd293bd6c
Princi Vershwal <vershwal.princi@gmail.com>
2025-03-17 12:46:14 +0530

    Modified query for adding publishe_at to feeds table

--- migrate/migrations/000022_add-published-at-to-feeds.up.sql
+++ migrate/migrations/000022_add-published-at-to-feeds.up.sql
@@ -1,7 +1,9 @@
-ALTER TABLE feeds ADD COLUMN published_at TIMESTAMP NOT NULL;
+ALTER TABLE feeds ADD COLUMN published_at TIMESTAMP;
 
 UPDATE feeds 
 JOIN posts ON feeds.post_id = posts.id 
 SET feeds.published_at = posts.published_at;
+
+ALTER TABLE feeds MODIFY COLUMN published_at TIMESTAMP NOT NULL;
 
 CREATE INDEX idx_published_at ON feeds(published_at);
