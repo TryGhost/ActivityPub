805aa1b3ad9bdedc7361eb329a655220bb946e66
Michael Barrett <mike@ghost.org>
2025-03-27 11:54:46 +0000

    Emit `post.liked` event when a post is liked (#442)
    ref https://linear.app/ghost/issue/AP-901
    
    Emit `post.liked` event when a post is liked so that we can listen on this event
    and create a corresponding notification for the post author.

---
+++ src/post/post-liked.event.ts
@@ -0,0 +1,20 @@
+import type { Post } from './post.entity';
+
+export class PostLikedEvent {
+    constructor(
+        private readonly post: Post,
+        private readonly accountId: number,
+    ) {}
+
+    getPost(): Post {
+        return this.post;
+    }
+
+    getAccountId(): number {
+        return this.accountId;
+    }
+
+    static getName(): string {
+        return 'post.liked';
+    }
+}
