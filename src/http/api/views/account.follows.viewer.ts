834ebd393c56ddfcebe9684dc6060e02046c178c
Princi Vershwal <vershwal.princi@gmail.com>
2025-04-10 11:23:09 +0530

    Cleaned up code

--- src/http/api/views/account.follows.viewer.ts
+++ src/http/api/views/account.follows.viewer.ts
@@ -1,7 +1,7 @@
 import type { Account } from 'account/account.entity';
 import { getAccountHandle } from 'account/utils';
+import type { FedifyContextFactory } from 'activitypub/fedify-context.factory';
 import type { Knex } from 'knex';
-import type { FedifyContextFactory } from '../../../activitypub/fedify-context.factory';
 
 /**
  * Maximum number of follow accounts to return
@@ -31,9 +31,6 @@ interface AccountRow {
 }
 
 export class AccountFollowsViewer {
-    /**
-     * @param db Database client
-     */
     constructor(
         private readonly db: Knex,
         private readonly fedifyContextFactory: FedifyContextFactory,
@@ -48,7 +45,6 @@ export class AccountFollowsViewer {
             throw new Error('Site default account not found');
         }
 
-        // Retrieve data
         const getAccounts =
             type === 'following'
                 ? this.getFollowingAccounts.bind(this)
@@ -99,16 +95,9 @@ export class AccountFollowsViewer {
         return accountFollowsView;
     }
 
-    /**
-     * Get the number of accounts that the provided account is following
-     *
-     * @param accountId id of the account
-     */
-    async getFollowingAccountsCount(accountId: number | null): Promise<number> {
-        if (!accountId) {
-            return 0;
-        }
-
+    private async getFollowingAccountsCount(
+        accountId: number,
+    ): Promise<number> {
         const result = await this.db('follows')
             .where('follower_id', accountId)
             .count('*', { as: 'count' });
@@ -116,16 +105,7 @@ export class AccountFollowsViewer {
         return Number(result[0].count);
     }
 
-    /**
-     * Get the number of accounts that are following the provided account
-     *
-     * @param accountId id of the account
-     */
-    async getFollowerAccountsCount(accountId: number | null): Promise<number> {
-        if (!accountId) {
-            return 0;
-        }
-
+    private async getFollowerAccountsCount(accountId: number): Promise<number> {
         const result = await this.db('follows')
             .where('following_id', accountId)
             .count('*', { as: 'count' });
@@ -133,16 +113,7 @@ export class AccountFollowsViewer {
         return Number(result[0].count);
     }
 
-    /**
-     * Get the accounts that are following the provided account
-     *
-     * The results are ordered in reverse chronological order
-     *
-     * @param accountId id of the account
-     * @param limit limit of the query
-     * @param offset offset of the query
-     */
-    async getFollowerAccounts(
+    private async getFollowerAccounts(
         accountId: number,
         limit: number,
         offset: number,
@@ -163,21 +134,11 @@ export class AccountFollowsViewer {
             // account id in descending order to ensure the most recent follows
             // are returned first (i.e in case multiple follows were created at
             // the same time)
-            // @TODO: Make this configurable via the options?
             .orderBy('follows.created_at', 'desc')
             .orderBy('accounts.id', 'desc');
     }
 
-    /**
-     * Get the accounts that the provided account is following
-     *
-     * The results are ordered in reverse chronological order
-     *
-     * @param accountId id of the account
-     * @param limit limit of the query
-     * @param offset offset of the query
-     */
-    async getFollowingAccounts(
+    private async getFollowingAccounts(
         accountId: number,
         limit: number,
         offset: number,
@@ -198,25 +159,14 @@ export class AccountFollowsViewer {
             // account id in descending order to ensure the most recent follows
             // are returned first (i.e in case multiple follows were created at
             // the same time)
-            // @TODO: Make this configurable via the options?
             .orderBy('follows.created_at', 'desc')
             .orderBy('accounts.id', 'desc');
     }
 
-    /**
-     * Check if an account is following another account
-     *
-     * @param accountId id of the account to check
-     * @param followeeAccountId: id of the followee account
-     */
-    async checkIfAccountIsFollowing(
-        accountId: number | null,
-        followeeAccountId: number | null,
+    private async checkIfAccountIsFollowing(
+        accountId: number,
+        followeeAccountId: number,
     ): Promise<boolean> {
-        if (!accountId || !followeeAccountId) {
-            return false;
-        }
-
         const result = await this.db('follows')
             .where('follower_id', accountId)
             .where('following_id', followeeAccountId)
