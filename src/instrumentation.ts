7f4921b862f2d1942e54acb726e1fcc587937390
Ricardo Pinto <hessian_loom_0u@icloud.com>
2025-04-11 16:30:20 +0100

    WIP

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,64 +1,64 @@
 console.log(`process uptime: ${Math.round(process.uptime() * 1000)}ms`);
 console.time('startup');
 console.time('instrumentation');
-import { DiagConsoleLogger, DiagLogLevel, diag } from '@opentelemetry/api';
-import {
-    BatchSpanProcessor,
-    SimpleSpanProcessor,
-} from '@opentelemetry/sdk-trace-base';
-import * as Sentry from '@sentry/node';
-
-if (process.env.NODE_ENV === 'production') {
-    if (process.env.OTEL_DEBUG_LOGGING) {
-        diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG);
-    } else {
-        diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.INFO);
-    }
-}
-
-if (process.env.SENTRY_DSN) {
-    const sentryClient = Sentry.init({
-        dsn: process.env.SENTRY_DSN,
-        environment: process.env.NODE_ENV || 'unknown',
-        release: process.env.K_REVISION,
-        tracesSampleRate: 1.0,
-        maxValueLength: 2000,
-    });
-
-    if (process.env.K_SERVICE) {
-        const { TraceExporter } = await import(
-            '@google-cloud/opentelemetry-cloud-trace-exporter'
-        );
-        sentryClient?.traceProvider?.addSpanProcessor(
-            new BatchSpanProcessor(new TraceExporter({})),
-        );
-    }
-
-    if (process.env.NODE_ENV === 'development') {
-        const { OTLPTraceExporter } = await import(
-            '@opentelemetry/exporter-trace-otlp-proto'
-        );
-        sentryClient?.traceProvider?.addSpanProcessor(
-            new SimpleSpanProcessor(
-                new OTLPTraceExporter({
-                    url: 'http://jaeger:4318/v1/traces',
-                }),
-            ),
-        );
-    }
-}
-
-export function spanWrapper<TArgs extends unknown[], TReturn>(
-    fn: (...args: TArgs) => TReturn,
-) {
-    return (...args: TArgs) => {
-        return Sentry.startSpan(
-            {
-                op: 'fn',
-                name: fn.name || 'anonymous',
-            },
-            () => fn(...args),
-        );
-    };
-}
+// import { DiagConsoleLogger, DiagLogLevel, diag } from '@opentelemetry/api';
+// import {
+//     BatchSpanProcessor,
+//     SimpleSpanProcessor,
+// } from '@opentelemetry/sdk-trace-base';
+// import * as Sentry from '@sentry/node';
+//
+// if (process.env.NODE_ENV === 'production') {
+//     if (process.env.OTEL_DEBUG_LOGGING) {
+//         diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG);
+//     } else {
+//         diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.INFO);
+//     }
+// }
+//
+// if (process.env.SENTRY_DSN) {
+//     const sentryClient = Sentry.init({
+//         dsn: process.env.SENTRY_DSN,
+//         environment: process.env.NODE_ENV || 'unknown',
+//         release: process.env.K_REVISION,
+//         tracesSampleRate: 1.0,
+//         maxValueLength: 2000,
+//     });
+//
+//     if (process.env.K_SERVICE) {
+//         const { TraceExporter } = await import(
+//             '@google-cloud/opentelemetry-cloud-trace-exporter'
+//         );
+//         sentryClient?.traceProvider?.addSpanProcessor(
+//             new BatchSpanProcessor(new TraceExporter({})),
+//         );
+//     }
+//
+//     if (process.env.NODE_ENV === 'development') {
+//         const { OTLPTraceExporter } = await import(
+//             '@opentelemetry/exporter-trace-otlp-proto'
+//         );
+//         sentryClient?.traceProvider?.addSpanProcessor(
+//             new SimpleSpanProcessor(
+//                 new OTLPTraceExporter({
+//                     url: 'http://jaeger:4318/v1/traces',
+//                 }),
+//             ),
+//         );
+//     }
+// }
+//
+// export function spanWrapper<TArgs extends unknown[], TReturn>(
+//     fn: (...args: TArgs) => TReturn,
+// ) {
+//     return (...args: TArgs) => {
+//         return Sentry.startSpan(
+//             {
+//                 op: 'fn',
+//                 name: fn.name || 'anonymous',
+//             },
+//             () => fn(...args),
+//         );
+//     };
+// }
 console.timeEnd('instrumentation');
