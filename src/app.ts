0865995577df2924bc5804f18401a30c05a6ebfd
Michael Barrett <mike@ghost.org>
2025-04-14 15:28:24 +0100

    Added custom webfinger handler
    ref https://linear.app/ghost/issue/AP-963
    
    Added a custom `GET /.well-known/webfinger` handler that will ensure that a
    resource on a `www.` domain can be referenced without the `www.` (i.e a
    webfinger lookup can be done on `@mike@mike.blog` if `mike.blog` is hosted at
    `www.mike.blog`)

--- src/app.ts
+++ src/app.ts
@@ -115,6 +115,7 @@ import {
     handleWebhookSiteChanged,
 } from './http/api';
 import { AccountFollowsView } from './http/api/views/account.follows.view';
+import { createWebFingerHandler } from './http/handler/webfinger';
 import { spanWrapper } from './instrumentation';
 import { KnexKvStore } from './knex.kvstore';
 import { scopeKvStore } from './kv-helpers';
@@ -901,6 +902,11 @@ function requireRole(...roles: GhostRole[]) {
         return next();
     };
 }
+
+app.get(
+    '/.well-known/webfinger',
+    spanWrapper(createWebFingerHandler(accountRepository, siteService)),
+);
 
 app.get(
     '/.ghost/activitypub/inbox/:handle',
---
+++ src/http/handler/webfinger.ts
@@ -0,0 +1,87 @@
+import type { Context as HonoContext, Next } from 'hono';
+
+import type { Account } from 'account/account.entity';
+import type { KnexAccountRepository } from 'account/account.repository.knex';
+import type { Site, SiteService } from 'site/site.service';
+
+const ACCOUNT_RESOURCE_PREFIX = 'acct:';
+const HOST_REGEX = /^([a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*\.[a-zA-Z0-9]+)$/;
+
+export function createWebFingerHandler(
+    accountRepository: KnexAccountRepository,
+    siteService: SiteService,
+) {
+    /**
+     * Custom webfinger implementation to allow resources hosted on the www
+     * version of a host to resolve to the non-www version of the host
+     *
+     * @see https://github.com/fedify-dev/fedify/blob/main/src/webfinger/handler.ts
+     */
+    return async function handleWebFinger(ctx: HonoContext, next: Next) {
+        const resource = ctx.req.query('resource');
+
+        // We only support custom handling of `acct:` resources - If the
+        // resource is not an `acct:` resource, fallback to the default
+        // webfinger implementation
+        if (!resource || !resource.startsWith(ACCOUNT_RESOURCE_PREFIX)) {
+            return next();
+        }
+
+        const [_, resourceHost] = resource
+            .slice(ACCOUNT_RESOURCE_PREFIX.length)
+            .split('@');
+        if (!resourceHost || !HOST_REGEX.test(resourceHost)) {
+            return next();
+        }
+
+        let site: Site | null = null;
+
+        site = await siteService.getSiteByHost(resourceHost);
+        if (site) {
+            // If a site is found for the resource host, we don't need to
+            // do any custom handling, we can just defer to the default
+            // webfinger implementation
+            return next();
+        }
+
+        site = await siteService.getSiteByHost(`www.${resourceHost}`);
+        if (!site) {
+            return new Response(null, {
+                status: 404,
+            });
+        }
+
+        let account: Account;
+
+        try {
+            account = await accountRepository.getBySite(site);
+        } catch (error) {
+            return new Response(null, {
+                status: 404,
+            });
+        }
+
+        const webfingerData = {
+            subject: `acct:${account.username}@${resourceHost}`,
+            aliases: [account.apId.toString()],
+            links: [
+                {
+                    rel: 'self',
+                    href: account.apId.toString(),
+                    type: 'application/activity+json',
+                },
+                {
+                    rel: 'http://webfinger.net/rel/profile-page',
+                    href: account.url.toString(),
+                },
+            ],
+        };
+
+        return new Response(JSON.stringify(webfingerData), {
+            status: 200,
+            headers: {
+                'Content-Type': 'application/jrd+json',
+            },
+        });
+    };
+}
