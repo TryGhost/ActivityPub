59dc08b8fc4362b6025470d6731e626cd8033124
Fabien O'Carroll <fabien@allou.is>
2025-04-15 11:14:22 +0700

    Added support for saving Account entities
    ref https://linear.app/ghost/issue/AP-1071
    
    This allows us to move away from the `updateAccount` implementation in
    AccountService which writes directly to the DB and then emits an
    `account.updated` event. Instead we can use the entity system and follow the
    existing patterns

---
+++ src/account/account-updated.event.ts
@@ -0,0 +1,13 @@
+import type { Account } from './account.entity';
+
+export class AccountUpdatedEvent {
+    static getName(): string {
+        return 'account.updated.event';
+    }
+
+    constructor(private readonly account: Account) {}
+
+    getAccount(): Account {
+        return this.account;
+    }
+}
