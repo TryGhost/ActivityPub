5dc4a8191e1d0cadf4a91733f7803e107e1ed905
Michael Barrett <mike@ghost.org>
2025-04-22 10:39:28 +0100

    Added database migrations for `blocks` table
    ref https://linear.app/ghost/issue/AP-1082
    
    Added a database table that will store a record of an account block. We will
    later query this table to determine if an account has blocked another account /
    is being blocked by another account.

---
+++ migrate/migrations/000040_add-blocks-table.up.sql
@@ -0,0 +1,15 @@
+CREATE TABLE blocks (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    created_at TIMESTAMP(6) NULL DEFAULT CURRENT_TIMESTAMP(6),
+
+    blocker_id INT UNSIGNED NOT NULL,
+    blocked_id INT UNSIGNED NOT NULL,
+
+    UNIQUE KEY unique_blocker_blocked (blocker_id, blocked_id),
+
+    FOREIGN KEY (blocker_id) REFERENCES accounts(id),
+    FOREIGN KEY (blocked_id) REFERENCES accounts(id),
+
+    KEY idx_blocks_blocker_id_blocked_id (blocker_id, blocked_id),
+    KEY idx_blocks_blocked_id_blocker_id (blocked_id, blocker_id)
+);
