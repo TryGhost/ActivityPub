3635a0125d946489ab815b0ee7a66632d365ce67
Michael Barrett <mike@ghost.org>
2025-04-24 10:38:18 +0100

    Use nullish coalescing for assignment

--- src/account/account.repository.knex.ts
+++ src/account/account.repository.knex.ts
@@ -24,12 +24,8 @@ export class KnexAccountRepository {
                 name: account.name,
                 bio: account.bio,
                 username: account.username,
-                avatar_url:
-                    account.avatarUrl == null ? null : account.avatarUrl?.href,
-                banner_image_url:
-                    account.bannerImageUrl == null
-                        ? null
-                        : account.bannerImageUrl?.href,
+                avatar_url: account.avatarUrl?.href ?? null,
+                banner_image_url: account.bannerImageUrl?.href ?? null,
             })
             .where({ id: account.id });
 
