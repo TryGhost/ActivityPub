99d3809fd01b4077dc8619484e9cae47f4820a64
Michael Barrett <mike@ghost.org>
2025-04-29 20:42:40 +0100

    Removed redundant post filtering from moderation service
    ref ref https://linear.app/ghost/issue/AP-1082
    
    Removed the logic added for poster filtering from the moderation service as this
    is now redundant

--- src/moderation/moderation.service.ts
+++ src/moderation/moderation.service.ts
@@ -1,32 +1,8 @@
-import type { Account } from 'account/account.entity';
-import type { PostDTO } from 'http/api/types';
 import type { Knex } from 'knex';
 import type { Post } from 'post/post.entity';
 
 export class ModerationService {
     constructor(private readonly db: Knex) {}
-
-    async filterBlockedPostsForAccount(account: Account, posts: PostDTO[]) {
-        if (posts.length === 0) {
-            return [];
-        }
-
-        const blockedAccountIds = new Set(
-            (
-                await this.db('blocks')
-                    .innerJoin('accounts', 'blocks.blocked_id', 'accounts.id')
-                    .select('accounts.ap_id')
-                    .where('blocks.blocker_id', account.id)
-            ).map((row) => row.ap_id),
-        );
-
-        return posts.filter(
-            (post) =>
-                !blockedAccountIds.has(post.author.id) &&
-                (post.repostedBy === null ||
-                    !blockedAccountIds.has(post.repostedBy.id)),
-        );
-    }
 
     async filterUsersForPost(
         userIds: number[],
