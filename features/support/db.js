14a0b38d93b3eecb3603e33d33962ad99cea5aa0
Michael Barrett <mike@ghost.org>
2025-05-01 08:55:20 +0100

    Refactored cucumber setup to allow for multiple step definition files (#601)
    no refs
    
    Miscellaneous refactorings to allow us to move to using multiple step definition
    files instead of one large file:
    
    - Introduced `support` directory to organize support files
        - Moved generic helper functions out of `stepdefs.js` and into their
        respective support files
    - Removed nearly all global state from `stepdefs.js`
        - Support files keep track of there global state (i.e `db.js` keeping track
        of database client instance)
    - Added `block_steps.js` to organize block related step definitions
    - Moved global static constants to environment variables
    
    I opted to just use the built in `World` rather than a custom one - We could
    maybe move to this at a later point and use it to keep track of global state etc

---
+++ features/support/db.js
@@ -0,0 +1,40 @@
+import Knex from 'knex';
+
+/** @type {import('knex').Knex} */
+let client;
+
+export function getClient() {
+    if (!client) {
+        client = Knex({
+            client: 'mysql2',
+            connection: {
+                host: process.env.MYSQL_HOST,
+                port: Number.parseInt(process.env.MYSQL_PORT),
+                user: process.env.MYSQL_USER,
+                password: process.env.MYSQL_PASSWORD,
+                database: process.env.MYSQL_DATABASE,
+                timezone: '+00:00',
+            },
+        });
+    }
+
+    return client;
+}
+
+export async function reset() {
+    const db = getClient();
+
+    await db.raw('SET FOREIGN_KEY_CHECKS = 0');
+    await db('key_value').truncate();
+    await db('notifications').truncate();
+    await db('feeds').truncate();
+    await db('blocks').truncate();
+    await db('follows').truncate();
+    await db('likes').truncate();
+    await db('reposts').truncate();
+    await db('posts').truncate();
+    await db('accounts').truncate();
+    await db('users').truncate();
+    await db('sites').truncate();
+    await db.raw('SET FOREIGN_KEY_CHECKS = 1');
+}
