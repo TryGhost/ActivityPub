584c95df6158830725f21f40da66a9298afc5d1c
Fabien O'Carroll <fabien@allou.is>
2025-05-05 12:06:29 +0800

    Refactored repost controller logic into service
    ref https://linear.app/ghost/issue/PROD-669
    
    By moving this logic all into the service we have a single place to add
    restrictions based on whether or not an account is blocked. We've also moved to
    the new Result type - which forces us to handle errors explicitly in the
    controller. The structure of the Activity we send has changed slightly, with
    just the object ID instead of the full object now - which has meant our tests
    had to be updated.

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -29,6 +29,8 @@ export type GetPostsError =
     | 'no-page-found'
     | 'not-an-actor';
 
+export type RepostError = GetByApIdError | 'already-reposted';
+
 export class PostService {
     constructor(
         private readonly postRepository: KnexPostRepository,
@@ -243,6 +245,32 @@ export class PostService {
         }
 
         post.addLike(account);
+
+        await this.postRepository.save(post);
+
+        return ok(post);
+    }
+
+    async repostByApId(
+        account: Account,
+        postId: URL,
+    ): Promise<Result<Post, RepostError>> {
+        const postToRepostResult = await this.getByApId(postId);
+
+        if (isError(postToRepostResult)) {
+            return postToRepostResult;
+        }
+
+        const post = getValue(postToRepostResult);
+
+        // We know this is not `null` because it just came from the DB
+        const reposted = await this.isRepostedByAccount(post.id!, account.id);
+
+        if (reposted) {
+            return error('already-reposted');
+        }
+
+        post.addRepost(account);
 
         await this.postRepository.save(post);
 
