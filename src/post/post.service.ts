e2c46a31da99b528bddd3c7fff01101cdaff6960
Princi Vershwal <vershwal.princi@gmail.com>
2025-05-13 18:32:26 +0530

    Adding mentions for Notes
    - Parse mentions from the Note/Reply content
    - If mentions are present, wrap it into hyperlinks and add to the db

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -11,12 +11,19 @@ import {
     isError,
     ok,
 } from 'core/result';
+import { lookupAPIdByHandle, lookupActorProfile } from 'lookup-helpers';
 import type { ModerationService } from 'moderation/moderation.service';
 import type {
     GCPStorageService,
     ImageVerificationError,
 } from 'storage/gcloud-storage/gcp-storage.service';
-import { Post, type PostAttachment, PostType } from './post.entity';
+import { ContentPreparer } from './content';
+import {
+    type Mention,
+    Post,
+    type PostAttachment,
+    PostType,
+} from './post.entity';
 import type { KnexPostRepository } from './post.repository.knex';
 
 export type GetByApIdError = 'upstream-error' | 'not-a-post' | 'missing-author';
@@ -176,6 +183,37 @@ export class PostService {
         return this.postRepository.isRepostedByAccount(postId, accountId);
     }
 
+    private async getMentionsFromContent(content: string): Promise<Mention[]> {
+        const ctx = this.fedifyContextFactory.getFedifyContext();
+        const mentions = ContentPreparer.parseMentions(content);
+        const processedMentions: Mention[] = [];
+
+        for (const mention of mentions) {
+            let account: Account | null = null;
+            const apId = await lookupAPIdByHandle(ctx, mention);
+            if (apId) {
+                const accountResult = await this.accountService.ensureByApId(
+                    new URL(apId),
+                );
+                if (isError(accountResult)) {
+                    continue;
+                }
+                account = getValue(accountResult);
+            }
+
+            const profileUrl = await lookupActorProfile(ctx, mention);
+            if (profileUrl && account) {
+                processedMentions.push({
+                    name: mention,
+                    href: profileUrl,
+                    account: account,
+                });
+            }
+        }
+
+        return processedMentions;
+    }
+
     async createNote(
         account: Account,
         content: string,
@@ -188,7 +226,9 @@ export class PostService {
             }
         }
 
-        const post = Post.createNote(account, content, image);
+        const mentions = await this.getMentionsFromContent(content);
+
+        const post = Post.createNote(account, content, image, mentions);
 
         await this.postRepository.save(post);
 
@@ -210,6 +250,8 @@ export class PostService {
             }
         }
 
+        const mentions = await this.getMentionsFromContent(content);
+
         const inReplyToResult = await this.getByApId(inReplyToId);
 
         if (isError(inReplyToResult)) {
@@ -227,7 +269,13 @@ export class PostService {
             return error('cannot-interact');
         }
 
-        const post = Post.createReply(account, content, inReplyTo, image);
+        const post = Post.createReply(
+            account,
+            content,
+            inReplyTo,
+            image,
+            mentions,
+        );
 
         await this.postRepository.save(post);
 
