c6ef8b89844eccdc16325ad191dbbc36ea7c9cf5
Fabien O'Carroll <fabien@allou.is>
2025-06-03 14:02:57 +0700

    Implemented `getAncestors` for ReplyChainView
    ref https://linear.app/ghost/issue/PROD-1935
    
    This is our first foray into using recursion in MySQL. What we're doing is
    building up a small in-memory table of the post ids we're interested in, and
    then selecting from the `posts` table based on a join to this in-memory table.
    
    The in-memory table is created by recursively joining `posts` onto itself via
    the `in_reply_to` column and the recursion is limited by a clause on `depth`
    
    We've also abstracted out the selection of a post row into a separate method,
    as we're going to need to reuse then when reading children and their replies.

---
+++ src/http/api/views/reply.chain.view.integration.test.ts
@@ -0,0 +1,63 @@
+import type { Knex } from 'knex';
+import type { Post } from 'post/post.entity';
+import { createTestDb } from 'test/db';
+import { type FixtureManager, createFixtureManager } from 'test/fixtures';
+import { beforeAll, beforeEach, describe, expect, it } from 'vitest';
+import { ReplyChainView } from './reply.chain.view';
+
+describe('ReplyChainView', () => {
+    let db: Knex;
+    let fixtureManager: FixtureManager;
+
+    beforeAll(async () => {
+        db = await createTestDb();
+        fixtureManager = createFixtureManager(db);
+    });
+
+    beforeEach(async () => {
+        await fixtureManager.reset();
+    });
+
+    describe('getAncestors', () => {
+        it('should return the paginated ancestors of a post', async () => {
+            const [account] = await fixtureManager.createInternalAccount();
+
+            let postsToCreate = ReplyChainView.MAX_ANCESTOR_DEPTH + 5;
+            const posts: Post[] = [];
+            while (postsToCreate-- > 0) {
+                const post = await fixtureManager.createPost(account, {
+                    inReplyTo: posts[posts.length - 1],
+                });
+                posts.push(post);
+            }
+
+            const allAncestors = posts.slice(0, -1);
+            const lastPost = posts[posts.length - 1];
+
+            const replyChainView = new ReplyChainView(db);
+            // @ts-expect-error Property 'getAncestors' is private and only accessible within class 'ReplyChainView'
+            const ancestors = await replyChainView.getAncestors(
+                account.id,
+                lastPost.apId,
+            );
+
+            expect(ancestors).toHaveLength(ReplyChainView.MAX_ANCESTOR_DEPTH);
+            expect(ancestors[0].post_ap_id).toBe(
+                allAncestors[
+                    allAncestors.length - ReplyChainView.MAX_ANCESTOR_DEPTH
+                ].apId.href,
+            );
+
+            // @ts-expect-error Property 'getAncestors' is private and only accessible within class 'ReplyChainView'
+            const remainingAncestors = await replyChainView.getAncestors(
+                account.id,
+                new URL(ancestors[0].post_ap_id),
+            );
+
+            expect(remainingAncestors).toHaveLength(4);
+            expect(remainingAncestors[0].post_ap_id).toBe(
+                allAncestors[0].apId.href,
+            );
+        });
+    });
+});
