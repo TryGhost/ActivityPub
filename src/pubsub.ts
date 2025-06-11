cb06a50cb048cfc151ad0cf03ba07196ba524ec7
Michael Barrett <mike@ghost.org>
2025-06-11 11:38:04 +0100

    Removed explicit pubsub topic / subscription check
    no ref
    
    Removed explicit pubsub topic / subscription check on app boot as they did not
    really provide much value (just because topics / subscriptions exist at time of
    boot doesn't mean they will still exist when we use them) and increased app
    boot time. If the client tries to publish to a topic that does not exist, an
    error will be thrown. We currently do not use subscriptions for anything (making
    the existence check redundant)

--- src/pubsub.ts
+++ src/pubsub.ts
@@ -13,67 +13,20 @@ interface InitClientConfig {
      * ID of the Pub/Sub project
      */
     projectId: string;
-    /**
-     * Array of Pub/Sub topics to publish messages to
-     */
-    topics: string[];
-    /**
-     * Array of Pub/Sub subscriptions to receive messages from
-     */
-    subscriptions: string[];
 }
 
 export function getFullTopic(projectId: string, topic: string) {
     return `projects/${projectId}/topics/${topic}`;
 }
 
-export async function initPubSubClient({
+export function initPubSubClient({
     host,
     isEmulator,
     projectId,
-    topics,
-    subscriptions,
 }: InitClientConfig) {
-    try {
-        // Initialise the Pub/Sub client
-        const pubSubClient = new PubSub({
-            apiEndpoint: host,
-            emulatorMode: isEmulator,
-            projectId,
-        });
-
-        // Check that the provided topics exist
-        const [existingTopics] = await pubSubClient.getTopics();
-
-        for (const topic of topics) {
-            const fullTopic = getFullTopic(pubSubClient.projectId, topic);
-
-            if (!existingTopics.some((t) => t.name === fullTopic)) {
-                throw new Error(`Topic [${topic}] does not exist`);
-            }
-        }
-
-        // Check that the provided subscriptions exist
-        const [existingSubscriptions] = await pubSubClient.getSubscriptions();
-
-        for (const subscription of subscriptions) {
-            const fullSubscription = `projects/${pubSubClient.projectId}/subscriptions/${subscription}`;
-
-            if (
-                !existingSubscriptions.some((s) => s.name === fullSubscription)
-            ) {
-                throw new Error(
-                    `Subscription [${subscription}] does not exist`,
-                );
-            }
-        }
-
-        return pubSubClient;
-    } catch (error) {
-        throw new Error(
-            `Failed to initialise Pub/Sub client: ${
-                error instanceof Error ? error.message : String(error)
-            }`,
-        );
-    }
+    return new PubSub({
+        apiEndpoint: host,
+        emulatorMode: isEmulator,
+        projectId,
+    });
 }
