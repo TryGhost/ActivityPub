f2e188cb5fe96fd477db11bf25833bc13c2db317
Sag <guptazy@gmail.com>
2025-06-13 16:14:48 +0200

    Trigger CI again to deploy to staging

--- src/storage/adapters/gcp-storage-adapter.ts
+++ src/storage/adapters/gcp-storage-adapter.ts
@@ -44,7 +44,7 @@ export class GCPStorageAdapter implements StorageAdapter {
         path: string,
     ): Promise<Result<string, StorageError>> {
         this.logging.info(
-            'save in GCPStorageAdapter called with file {file} and path {path}',
+            'Save in GCPStorageAdapter called with file {file} and path {path}',
             {
                 file: file.name,
                 path,
