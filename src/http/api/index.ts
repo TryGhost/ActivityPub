4717e8fd60b21dc8d91af82acaad5e6c9f428889
Fabien O'Carroll <fabien@allou.is>
2025-07-04 11:18:09 +0700

    refactor: rename post.ts to post.controller.ts

--- src/http/api/index.ts
+++ src/http/api/index.ts
@@ -2,7 +2,7 @@ export { AccountController } from './account.controller';
 export * from './feed.controller';
 export * from './notification.controller';
 export * from './note';
-export * from './post';
+export * from './post.controller';
 export * from './search.controller';
 export * from './image-upload';
 export * from './webhook.controller';
