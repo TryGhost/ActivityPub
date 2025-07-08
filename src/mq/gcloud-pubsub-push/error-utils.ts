86ceeb076899a234e675766a74a96248bb223aeb
Michael Barrett <mike@ghost.org>
2025-07-08 15:38:56 +0100

    Added handling of non-standard HTTP codes in error analysis (#1003)
    ref https://linear.app/ghost/issue/PROD-2210
    ref https://github.com/TryGhost/ActivityPub/pull/994
    
    Added handling for non-standard HTTP codes in error analysis - Any error with
    a non-standard HTTP code will be treated as a permanent failure and will not be
    retried

--- src/mq/gcloud-pubsub-push/error-utils.ts
+++ src/mq/gcloud-pubsub-push/error-utils.ts
@@ -60,7 +60,29 @@ function analyzeFedifyDeliveryError(error: Error): ErrorAnalysis {
     // Extract status code from the Fedify delivery error
     const statusCode = Number.parseInt(fedifyMatch[1], 10);
 
-    const permanentFailureCodes = [
+    const standardStatusCodes = new Set([
+        // 1xx Informational
+        100, 101, 102, 103, 104,
+        // 2xx Success
+        200, 201, 202, 203, 204, 205, 206, 207, 208, 226,
+        // 3xx Redirection
+        300, 301, 302, 303, 304, 305, 306, 307, 308,
+        // 4xx Client Error
+        400, 401, 402, 403, 404, 405, 406, 407, 408, 409, 410, 411, 412, 413,
+        414, 415, 416, 417, 421, 422, 423, 424, 425, 426, 428, 429, 431, 451,
+        // 5xx Server Error
+        500, 501, 502, 503, 504, 505, 506, 507, 508, 511,
+    ]);
+
+    // Non-standard status codes are not retryable and not reportable
+    if (!standardStatusCodes.has(statusCode)) {
+        return {
+            isRetryable: false,
+            isReportable: false,
+        };
+    }
+
+    const permanentFailureStatusCodes = [
         400, // Bad Request
         401, // Unauthorized
         403, // Forbidden
@@ -71,7 +93,7 @@ function analyzeFedifyDeliveryError(error: Error): ErrorAnalysis {
         501, // Not Implemented
     ];
 
-    const isRetryable = !permanentFailureCodes.includes(statusCode);
+    const isRetryable = !permanentFailureStatusCodes.includes(statusCode);
 
     return {
         isRetryable,
