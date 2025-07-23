94b58b8b0f9970904aa65e1ea53a6803ca9bb0a0
Fabien O'Carroll <fabien@allou.is>
2025-07-23 13:28:05 +0400

    Implemented steps for second test

--- features/step_definitions/internal_account_steps.js
+++ features/step_definitions/internal_account_steps.js
@@ -88,19 +88,64 @@ Then('the article is in my followers feeds', async function () {
     }
 });
 
-When('I create a note which mentions alice', async () => {
-    // Write code here that turns the phrase above into concrete actions
-    return 'pending';
+When('I create a note which mentions alice', async function () {
+    const body = JSON.stringify({
+        content: 'Hey @index@alice.test!',
+    });
+
+    const response = await fetchActivityPub(
+        'https://self.test/.ghost/activitypub/v1/actions/note',
+        {
+            method: 'POST',
+            body: body,
+        },
+    );
+
+    const json = await response.json();
+
+    this.note = json.post;
 });
 
-Then('the note is in my followers feeds', async () => {
-    // Write code here that turns the phrase above into concrete actions
-    return 'pending';
+Then('the note is in my followers feeds', async function () {
+    const feeds = await Promise.all([
+        fetchActivityPub(
+            'https://alice.test/.ghost/activitypub/v1/feed/notes',
+            { method: 'GET' },
+        ),
+        fetchActivityPub('https://bob.test/.ghost/activitypub/v1/feed/notes', {
+            method: 'GET',
+        }),
+        fetchActivityPub(
+            'https://carol.test/.ghost/activitypub/v1/feed/notes',
+            { method: 'GET' },
+        ),
+    ]);
+
+    const noteId = this.note.id;
+
+    for (const feed of feeds) {
+        const json = await feed.json();
+        assert(
+            json.posts.find((post) => post.id === noteId),
+            'Note is not in feed',
+        );
+    }
 });
 
-Then('alice recieves a mention notification', async () => {
-    // Write code here that turns the phrase above into concrete actions
-    return 'pending';
+Then('alice recieves a mention notification', async function () {
+    const notifications = await fetchActivityPub(
+        'https://alice.test/.ghost/activitypub/v1/notifications',
+        { method: 'GET' },
+    );
+    const json = await notifications.json();
+    assert(
+        json.notifications.find(
+            (notification) =>
+                notification.type === 'mention' &&
+                notification.post.id === this.note.id,
+        ),
+        'Notification is not in notifications',
+    );
 });
 
 When('I create a reply to alice', async () => {
