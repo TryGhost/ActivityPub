67f1861795725dd4bf152fd1b3f06ef881f6fcd2
Fabien O'Carroll <fabien@allou.is>
2025-07-23 18:15:59 +0400

    Added handling for `SocketError other side closed`
    We're seeing this in the wild too, and want to catch it

--- src/mq/gcloud-pubsub-push/error-utils.ts
+++ src/mq/gcloud-pubsub-push/error-utils.ts
@@ -104,7 +104,8 @@ function isNetworkConnectivityError(error: Error, depth = 0): boolean {
         error.message.match(/connect ECONNREFUSED/i) !== null ||
         error.message.match(/connect ECONNRESET/i) !== null ||
         error.message.match(/socket hang up/i) !== null ||
-        error.message.match(/Connect Timeout Error/i) !== null
+        error.message.match(/Connect Timeout Error/i) !== null ||
+        error.message.match(/other side closed/i) !== null
     ) {
         return true;
     }
