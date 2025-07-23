eec0c7c6e57503d8a7540795cc310a35604348c1
Fabien O'Carroll <fabien@allou.is>
2025-07-23 12:44:40 +0400

    Added pending snippets

---
+++ features/step_definitions/internal_account_steps.js
@@ -0,0 +1,106 @@
+import { Given, When, Then } from '@cucumber/cucumber';
+
+Given('I have internal account followers', async () => {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+When('I create a post in ghost', async () => {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Then('an article is in my followers feeds', async () => {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Given('I have internal account followers', async () => {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+When('I create a note which mentions alice', async () => {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Then('the note is in my followers feeds', async () => {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Then('alice recieves a mention notification', async () => {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Given('I have internal account followers', async () => {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+When('I create a reply to alice', async () => {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Then('the reply is not in my followers feeds', async () => {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Then('alice recieves a reply notification', async () => {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Given('I have internal account followers', async () => {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+When('I delete a note', async () => {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Then('the note is not in my followers feeds', async () => {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Given('I have internal account followers', async () => {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+When('I like alices note', async () => {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Then('alice recieves a like notification', async () => {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Given('I have internal account followers', async () => {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+When('I repost alices note', async () => {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Then('the note is in my followers feeds', async () => {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Then('alice recieves a repost notification', async () => {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
