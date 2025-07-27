237f225258e4c13ff3edc65f027a03ac08247e5f
Fabien O'Carroll <fabien@allou.is>
2025-07-27 23:38:59 +0700

    fixup! Logging out the X-Cloud-Trace-Context header

--- src/app.ts
+++ src/app.ts
@@ -525,9 +525,14 @@ const tracer = otelApi.trace.getTracer('activitypub', '1.0.0');
 app.get('/.ghost/activitypub/trace-testing', async (ctx) => {
     try {
         if (ctx.req.query('X-Cloud-Trace-Context')) {
-            globalLogging.info('X-Cloud-Trace-Context {X-Cloud-Trace-Context}', {
-                'X-Cloud-Trace-Context': ctx.req.header('X-Cloud-Trace-Context'),
-            });
+            globalLogging.info(
+                'X-Cloud-Trace-Context {X-Cloud-Trace-Context}',
+                {
+                    'X-Cloud-Trace-Context': ctx.req.header(
+                        'X-Cloud-Trace-Context',
+                    ),
+                },
+            );
         }
 
         let activeContext = otelApi.context.active();
