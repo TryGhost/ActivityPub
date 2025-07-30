b56abf66ee8bb09ded47fc055174f2cf8d0adfe7
Michael Barrett <mike@ghost.org>
2025-07-30 15:19:04 +0100

    Fixed cucumber tests

--- features/step_definitions/site_steps.js
+++ features/step_definitions/site_steps.js
@@ -3,10 +3,8 @@ import { Given, When } from '@cucumber/cucumber';
 import { getClient } from '../support/db.js';
 import { fetchActivityPub } from '../support/request.js';
 
-Given('there is no entry in the sites table', async function () {
+Given('there is no entry in the sites table', async () => {
     await getClient()('sites').del();
-
-    this.SITE_ID = null;
 });
 
 When('we request the site endpoint', async function () {
