ee2fd09402641535c024613b70d2b7868639431c
Sag <guptazy@gmail.com>
2025-08-27 09:01:14 +0200

    Updated API response for client-config

--- src/http/api/client-config.controller.ts
+++ src/http/api/client-config.controller.ts
@@ -4,12 +4,15 @@ import { APIRoute } from '@/http/decorators/route.decorator';
 export class ClientConfigController {
     @APIRoute('GET', 'client-config', 'stable')
     async handleGetClientConfig(_ctx: AppContext) {
-        const clientConfig = {
-            requiredVersion: '^1.0.0',
-            cdnUrl: 'https://cdn.jsdelivr.net/ghost/admin-x-activitypub@1/dist/admin-x-activitypub.js',
+        const major = 0;
+        const name = 'admin-x-activitypub';
+        const client = {
+            name,
+            requiredVersion: `^${major}.0.0`,
+            cdnUrl: `https://cdn.jsdelivr.net/ghost/${name}@${major}/dist/${name}.js`,
         };
 
-        return new Response(JSON.stringify(clientConfig), {
+        return new Response(JSON.stringify({ client }), {
             status: 200,
             headers: {
                 'Content-Type': 'application/json',
